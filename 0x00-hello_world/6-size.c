cad2b0714e5f1706b108853b89cebbb3b51a14a6
Edwinogechi <edwinogechi96@gmail.com>
2023-02-17 00:35:06 -0800

    Size varies

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,13 +11,14 @@ int main(void)
 	char a;
 	int b;
 	long int c;
+	long long int d;
 	float f;
 
-	printf("Size of a char: %lu byte(s)", sizeof(char));
-	printf("Size of an int: %lu byte(s)", sizeof(int));
-	printf("Size of a long int: %lu byte(s)", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)", sizeof(float));
+	printf("Size of a char: %lu byte(s)", sizeof(a));
+	printf("Size of an int: %lu byte(s)", sizeof(b));
+	printf("Size of a long int: %lu byte(s)", sizeof(c));
+	printf("Size of a long long int: %lu byte(s)", sizeof(d));
+	printf("Size of a float: %lu byte(s)", sizeof(f));
 	return (0);
 
 }
