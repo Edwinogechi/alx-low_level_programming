a95aa4b052308f67338f3af9079b1556fb3ec2a0
Edwinogechi <edwinogechi96@gmail.com>
2023-02-21 08:57:34 -0800

    Task 0 putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - major function is to print _putchar
+ * _putchar printed by me
+ * Return: always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
