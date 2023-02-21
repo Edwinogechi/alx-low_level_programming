d61dcb82b665f7986d49357e87f208d70b74c95e
Edwinogechi <edwinogechi96@gmail.com>
2023-02-21 15:54:11 -0800

    task 10 addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - perfoms the adding function of two numbers
+ *
+ * @i: parameter 1
+ * @j: parameter 2
+ * Return: sum of two numbers
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+
+}
