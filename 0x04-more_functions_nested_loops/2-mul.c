c12a15e285101db297f4f70d35d31bf40c20f319
Edwinogechi <edwinogechi96@gmail.com>
2023-02-23 10:28:48 -0800

    task 3 on multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function multiplying two intergers
+ * @a: interger 1
+ * @b: interger 2
+ * Return: the result of multiplication done
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
