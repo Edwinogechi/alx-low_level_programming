eec89f444f321a05e64bc60850b5c60db2a080d8
Edwinogechi <edwinogechi96@gmail.com>
2023-02-23 09:15:14 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks digits from 0 to 9
+ * @c: parameter tor be checked
+ *
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+
+}
