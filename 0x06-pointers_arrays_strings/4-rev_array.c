3ec13d29f41b76b286d778fde7f2eac9fc5a76b5
Edwinogechi <edwinogechi96@gmail.com>
2023-03-01 11:24:08 -0800

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,7 +3,7 @@
 /**
  * reverse_array - Reverses an array of integers.
  * @a: array to be reversed.
- * @n: number of elements in the array.
+ * @n: number of elements in the array
  */
 
 void reverse_array(int *a, int n)
