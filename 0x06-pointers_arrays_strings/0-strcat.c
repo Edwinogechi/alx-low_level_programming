88a1ae40587e57b87aa2aaf7a0aa635d88da40f0
Edwinogechi <edwinogechi96@gmail.com>
2023-03-01 08:59:30 -0800

    hot fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 
 /**
  * _strcat - a function strcat
@@ -9,14 +9,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int len = 0, 1;
+	int len = 0, i;
 
 	while (dest[len])
 		len++;
 
 	for (i = 0 ; src[i] != '\0' ; i++)
 	{
-		dest[len] = scrn[i];
+		dest[len] = src[i];
 		len += 1;
 	}
 	dest[len] = '\0';
